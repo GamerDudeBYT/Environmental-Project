078b13393910c2e44cc38ad0bb4a9ae9ca95c0f5
GamerDudeBYT <ethanbell15@outlook.com>
2024-11-16 13:24:30 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const random_tasks = {
+		easy: [
+			{
+				name: "Example Easy Random Task",
+				description: "Example Easy Random Task description. Try to limit to 3 sentences maximum."
+			}
+		],
+
+		medium: [
+			{
+				name: "Example Medium Random Task",
+				description: "Example Medium Random Task description. Try to limit to 3 sentences maximum."
+			}
+		],
+
+		hard: [
+			{
+				name: "Example Hard Random Task",
+				description: "Example Hard Random Task description. Try to limit to 3 sentences maximum."
+			}
+		]
+	}
+})
