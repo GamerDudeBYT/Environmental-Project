988cd34c31aa0856fcdddec966ca2a7b6e3e30d2
GamerDudeBYT <ethanbell15@outlook.com>
2024-11-27 14:56:23 +0000

    might work

--- navbar.js
+++ navbar.js
@@ -1,4 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
+	const home_div = document.getElementById("home");
+	const about_div = document.getElementById("about");
+
 	/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 	const navbar_func = () => {
 		var x = document.getElementById("topnav");
@@ -8,4 +11,15 @@ document.addEventListener("DOMContentLoaded", () => {
 			x.className = "navbar";
 		}
 	}
+
+	const home_func = () => {
+		about_div.style.cssText = "display: none;";
+		home_div.style.cssText = "display: block;";
+	};
+
+	const about_func = () => {
+		home_div.style.cssText = "display: none;";
+		about_div.style.cssText = "display: block;";
+	};
+
 });
