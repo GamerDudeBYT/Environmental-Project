0d85acbb3309bd50346a0a72d5c88912fb7271d0
GamerDudeBYT <ethanbell15@outlook.com>
2024-12-04 15:17:36 +0000

    please work

--- scripts/includes.js
+++ scripts/includes.js
@@ -1,23 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
 	function includeHTML() {
-		const elements = document.querySelectorAll("[include-html]");
-		elements.forEach((element) => {
-			const file = element.getAttribute("include-html");
+		var z, i, elmnt, file, xhttp;
+		/* Loop through a collection of all HTML elements: */
+		z = document.getElementsByTagName("*");
+		for (i = 0; i < z.length; i++) {
+			elmnt = z[i];
+			/*search for elements with a certain atrribute:*/
+			file = elmnt.getAttribute("include-html");
 			if (file) {
-				try {
-					const response = fetch(file);
-					if (response.ok) {
-						element.innerHTML = response.text();
-					} else {
-						element.innerHTML = "Page not found.";
+				/* Make an HTTP request using the attribute value as the file name: */
+				xhttp = new XMLHttpRequest();
+				xhttp.onreadystatechange = function () {
+					if (this.readyState == 4) {
+						if (this.status == 200) { elmnt.innerHTML = this.responseText; }
+						if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
+						/* Remove the attribute, and call this function once more: */
+						elmnt.removeAttribute("include-html");
+						includeHTML();
 					}
-					element.removeAttribute("include-html");
-				} catch (error) {
-					element.innerHTML = "Error loading the content.";
 				}
-				includeHTML();
+				xhttp.open("GET", file, true);
+				xhttp.send();
+				/* Exit the function: */
+				return;
 			}
-		});
+		}
 	}
-	includeHTML();
 })
